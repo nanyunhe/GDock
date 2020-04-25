bf5bc54e769b856b15f5b46616018b3ddff9bb1a
kyarazhan <shangrilas@live.com>
2020-04-25 18:41:10 +0800

    删除

--- lean_diy.sh
+++ lean_diy.sh
@@ -10,6 +10,5 @@ sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 sed -i 's/Openwrt/KyaraSSR/g' package/base-files/files/bin/config_generate
 sed -i 's/Openwrt/Kyara/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 git clone https://github.com/fw876/helloworld package/SSR_Plus
-git clone https://github.com/pymumu/smartdns package/smartdns
 ./scripts/feeds update -a
 ./scripts/feeds install -a
